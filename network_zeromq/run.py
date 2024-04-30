5c8900f021749ce1c26321e3dc0724149426ee15
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2024-04-30 10:55:37 +0200

    Add basic node structure

--- network_zeromq/run.py
+++ network_zeromq/run.py
@@ -9,9 +9,22 @@ only sends strings, not the messages.
 # Imports & Globals
 ######################
 
+import sys
+
+from autopsy.core import Core
+from autopsy.node import Node
+
 import zmq
 
 print ("""Loaded zmq.
 \tZeroMQ version: %s
 \tpyzmq version: %s
 """ % (zmq.zmq_version(), zmq.pyzmq_version()))
+
+
+if __name__ == "__main__":
+    Core.init(args = sys.argv)
+
+    n = Node("network_zeromq")
+
+    Core.spin(n)
