6a4916505180cd14c24aed0457cb2853882c7690
Jaroslav Klapálek <klapajar@fel.cvut.cz>
2024-05-16 13:26:51 +0200

    Define arguments

--- network_zeromq/run.py
+++ network_zeromq/run.py
@@ -16,10 +16,52 @@ from autopsy.node import Node
 
 import zmq
 
+import argparse
+
 print ("""Loaded zmq.
 \tZeroMQ version: %s
 \tpyzmq version: %s
 """ % (zmq.zmq_version(), zmq.pyzmq_version()))
+
+PARSER = argparse.ArgumentParser(
+    prog = "run.py",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description = """
+Create a network connection between two devices.
+    """,  # noqa: E501
+)
+
+PARSER.add_argument(
+    "ip_address",
+
+    help = "IP address of this computer, %%s",
+    type = str,
+    metavar = "LOCAL_IP_ADDRESS",
+)
+
+PARSER.add_argument(
+    "port",
+
+    help = "port number of local socket, %%d",
+    type = int,
+    metavar = "LOCAL_PORT",
+)
+
+PARSER.add_argument(
+    "remote_ip",
+
+    help = "IP address of other computer, %%d",
+    type = str,
+    metavar = "REMOTE_IP_ADDRESS",
+)
+
+PARSER.add_argument(
+    "remote_port",
+
+    help = "port number to connect to, %%d",
+    type = int,
+    metavar = "REMOTE_PORT",
+)
 
 
 ######################
@@ -45,7 +87,7 @@ class NetworkNode(Node):
 ######################
 
 if __name__ == "__main__":
-    # TODO: Handle arguments
+    args, other = PARSER.parse_known_args()
 
     Core.init(args = sys.argv)
 
